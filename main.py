9c9c1551414e621666d0614c25b95e5f18d67240
Momoha <mdmohinpasha@gmail.com>
2022-12-29 15:05:29 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import turtle
+from turtle import Turtle, Screen, colormode
+import random
+
+t = Turtle()
+s = Screen()
+
+
+# t.shape("turtle")
+
+# Draw a square
+# t.pencolor("red")
+# for i in range(4):
+#     t.forward(100)
+#     t.right(90)
+
+# Draw spaces lines
+# for i in range(10):
+#     t.forward(10)
+#     t.penup()
+#     t.forward(10)
+#     t.pendown()
+
+# Random shapes from Triangle to shape with 10 sides
+# def shape_creator(sides):
+#     angle = 360/sides
+#     for i in range(sides):
+#         t.forward(100)
+#         t.right(angle)
+#
+#
+# colormode(255)
+# t.pensize(3)
+# for shape in range(3, 11):
+#     tup = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+#     print(tup)
+#     shape_creator(shape)
+#     t.color(tup)
+#
+# s.exitonclick()
+
+# Random walk
+t.pensize(5)
+colormode(255)
+directions = [0, 90, 180, 270]
+for walk in range(100):
+    t.speed(0)
+    tup = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    t.color(tup)
+    t.setheading(random.choice(directions))
+    t.forward(20)
+
